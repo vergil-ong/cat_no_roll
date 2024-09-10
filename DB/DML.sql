9e10fb1bc84cfa30750744706b9d009a292149bc
vergil-ong <865208597@qq.com>
2024-09-10 16:25:27 +0800

    add sql

---
+++ DB/DML.sql
@@ -0,0 +1,63 @@
+insert into file_addr(`addr`, `whole_addr`, `file_type`, `id`) values ('ADMIN_CAT_NO_ROLL/before1.jpg','2','1','1')
+
+insert into file_addr(`addr`, `whole_addr`, `file_type`, `id`)
+values ('ADMIN_CAT_NO_ROLL/before2.jpg','2','1','2')
+
+insert into file_addr(`addr`, `whole_addr`, `file_type`, `id`)
+  values ('ADMIN_CAT_NO_ROLL/before1.mp4','2','2','3'),
+    ('ADMIN_CAT_NO_ROLL/before1_img.png','2','1','4'),
+('ADMIN_CAT_NO_ROLL/before2.mp4','2','2','5'),
+('ADMIN_CAT_NO_ROLL/before2_img.png','2','1','6')
+
+insert into file_addr(`addr`, `whole_addr`, `file_type`, `id`)
+values ('ADMIN_CAT_NO_ROLL/install1.mp4','2','2','34'),
+    ('ADMIN_CAT_NO_ROLL/install2.mp4','2','2','35'),
+    ('ADMIN_CAT_NO_ROLL/install3.mp4','2','2','36'),
+    ('ADMIN_CAT_NO_ROLL/install4.mp4','2','2','37'),
+    ('ADMIN_CAT_NO_ROLL/install1_img.png','2','1','38'),
+    ('ADMIN_CAT_NO_ROLL/install2_img.png','2','1','39'),
+    ('ADMIN_CAT_NO_ROLL/install3_img.png','2','1','40'),
+    ('ADMIN_CAT_NO_ROLL/install4_img.png','2','1','41')
+
+insert into file_addr(`addr`, `whole_addr`, `file_type`, `id`)
+values ('ADMIN_CAT_NO_ROLL/display1.jpg','2','1','68'),
+    ('ADMIN_CAT_NO_ROLL/display2.jpg','2','1','69'),
+    ('ADMIN_CAT_NO_ROLL/display3.jpg','2','1','70'),
+    ('ADMIN_CAT_NO_ROLL/display4.jpg','2','1','71'),
+    ('ADMIN_CAT_NO_ROLL/display5.jpg','2','1','72'),
+    ('ADMIN_CAT_NO_ROLL/display6.jpg','2','1','73'),
+    ('ADMIN_CAT_NO_ROLL/display1.mp4','2','2','80'),
+    ('ADMIN_CAT_NO_ROLL/display2.mp4','2','2','81'),
+    ('ADMIN_CAT_NO_ROLL/display3.mp4','2','2','82'),
+    ('ADMIN_CAT_NO_ROLL/display4.mp4','2','2','83'),
+    ('ADMIN_CAT_NO_ROLL/display5.mp4','2','2','84'),
+    ('ADMIN_CAT_NO_ROLL/display6.mp4','2','2','85'),
+    ('ADMIN_CAT_NO_ROLL/display1_img.png','2','1','86'),
+    ('ADMIN_CAT_NO_ROLL/display2_img.png','2','1','87'),
+    ('ADMIN_CAT_NO_ROLL/display3_img.png','2','1','88'),
+    ('ADMIN_CAT_NO_ROLL/display4_img.png','2','1','89'),
+    ('ADMIN_CAT_NO_ROLL/display5_img.png','2','1','90'),
+    ('ADMIN_CAT_NO_ROLL/display6_img.png','2','1','91'),
+    ('ADMIN_CAT_NO_ROLL/dispose1.mp4','2','2','104'),
+    ('ADMIN_CAT_NO_ROLL/dispose2.mp4','2','2','105'),
+    ('ADMIN_CAT_NO_ROLL/dispose3.mp4','2','2','106'),
+    ('ADMIN_CAT_NO_ROLL/dispose4.mp4','2','2','107'),
+    ('ADMIN_CAT_NO_ROLL/dispose5.mp4','2','2','108'),
+    ('ADMIN_CAT_NO_ROLL/dispose6.mp4','2','2','109'),
+    ('ADMIN_CAT_NO_ROLL/dispose1_img.png','2','1','110'),
+    ('ADMIN_CAT_NO_ROLL/dispose2_img.png','2','1','111'),
+    ('ADMIN_CAT_NO_ROLL/dispose3_img.png','2','1','112'),
+    ('ADMIN_CAT_NO_ROLL/dispose4_img.png','2','1','113'),
+    ('ADMIN_CAT_NO_ROLL/dispose5_img.png','2','1','114'),
+    ('ADMIN_CAT_NO_ROLL/dispose6_img.png','2','1','115')
+
+
+insert into user_upload_info(wechat_code, before_img_1, before_img_2, before_video_1, before_video_img_1, before_video_2, before_video_img_2,
+                             install_video_1, install_video_img_1, install_video_2, install_video_img_2, install_video_3, install_video_img_3, install_video_4, install_video_img_4,
+                             display_img_1, display_img_2, display_img_3, display_img_4, display_img_5, display_img_6,
+                             display_video_1, display_video_img_1, display_video_2, display_video_img_2, display_video_3, display_video_img_3,
+                             display_video_4, display_video_img_4, display_video_5, display_video_img_5, display_video_6, display_video_img_6,
+                             dispose_video_1, dispose_video_img_1, dispose_video_2, dispose_video_img_2, dispose_video_3, dispose_video_img_3,
+                             dispose_video_4, dispose_video_img_4, dispose_video_5, dispose_video_img_5, dispose_video_6, dispose_video_img_6,
+                             ID, file_change)
+values('ADMIN_CAT_NO_ROLL',1, 2, 3, 4, 5, 6, 34, 38, 35, 39, 36, 40, 37, 41, 68,69,70,71,72,73, 80,86,81,87, 82,88,83,89, 84,90,85,91, 104,110,105,111,106,112,107,113,108,114,109,115, 1,0)
