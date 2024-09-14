11428aca5f4b61a6b09e577ef32aa5fe56f0b1c7
vergil-ong <865208597@qq.com>
2024-09-14 16:43:54 +0800

    add

---
+++ cat-upload-file3/src/main/java/com/github/ong/utils/StringUtil.java
@@ -0,0 +1,15 @@
+package com.github.ong.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.UUID;
+
+@Slf4j
+public class StringUtil {
+
+    public static final String BLANK = "";
+
+    public static String uuid() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+}
